ae21584c521d12a3f684fc0c1c5bc0a5c5826863
gaox <thisisgaox@qq.com>
2023-10-22 11:38:48 +0800

    Initial commit

--- src/main/java/com/example/danger_manage/DangerManageApplication.java
+++ src/main/java/com/example/danger_manage/DangerManageApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DangerManageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DangerManageApplication.class, args); //123567
+        SpringApplication.run(DangerManageApplication.class, args); //123567gaox
     }
 }
