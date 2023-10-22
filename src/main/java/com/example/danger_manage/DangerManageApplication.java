ac57eeb076b0a46fa97858f1f15d5bc48f46a74a
gaox <thisisgaox@qq.com>
2023-10-22 11:25:04 +0800

    Initial commit

---
+++ src/main/java/com/example/danger_manage/DangerManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.danger_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangerManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangerManageApplication.class, args);
+    }
+    //gaox123
+}
---
+++ src/test/java/com/example/danger_manage/DangerManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danger_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangerManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
