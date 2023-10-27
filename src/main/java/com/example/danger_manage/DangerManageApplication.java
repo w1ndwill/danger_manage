ef08bc936f079cd5ecce33b5b78f6b30caf1f492
gaox <thisisgaox@qq.com>
2023-10-27 13:45:53 +0800

    Initial commit

--- src/main/java/com/example/danger_manage/DangerManageApplication.java
+++ src/main/java/com/example/danger_manage/DangerManageApplication.java
@@ -8,5 +8,5 @@ public class DangerManageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DangerManageApplication.class, args);
-    } //123
+    } //12345
 }
