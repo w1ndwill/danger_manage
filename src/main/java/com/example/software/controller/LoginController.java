7b5467bea67c325913edbe000b582bf942eb83a9
zhanghaiyang-dh <2191928600@qq.com>
2023-11-10 13:58:40 +0800

    12

--- src/main/java/com/example/software/controller/LoginController.java
+++ src/main/java/com/example/software/controller/LoginController.java
@@ -3,10 +3,8 @@ package com.example.software.controller;
 
 import com.example.software.bean.UserBean;
 import com.example.software.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,13 +22,22 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping("/Index") // 映射到前端页面的URL
+    @RequestMapping("/task") // 映射到前端页面的URL
     public String getIndexPage() {
-        return "Index"; // 这里的字符串是前端页面的名称，Spring Boot将根据它返回前端页面
+        return "mytask"; // 这里的字符串是前端页面的名称，Spring Boot将根据它返回前端页面
     }
     @RequestMapping("/巡查计划")
     public String showInspectionPlan() {
         return "login"; // 返回与巡查计划相关的视图
+    }
+    @RequestMapping("/ts")
+    public String showts() {
+        return "ts";
+    }
+
+    @RequestMapping("/alltask")
+    public String showt() {
+        return "alltask";
     }
 
     @RequestMapping(value = "/loginIn", method = RequestMethod.POST)
@@ -40,7 +47,7 @@ public class LoginController {
         String tn = userService.getIdByUsernameAndPassword(name, password);
         modelAndView.addObject("tn", tn); // 将tn存储在Model中
         if (userBean != null) {
-            modelAndView.setViewName("main");
+            modelAndView.setViewName("main1");
         } else {
             modelAndView.setViewName("error");
         }
