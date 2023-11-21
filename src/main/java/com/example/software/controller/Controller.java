525a38e931dbe70e83edcd5437e4ce2b8b043b8a
zhanghaiyang-dh <2191928600@qq.com>
2023-11-21 19:57:56 +0800

    zhy

--- src/main/java/com/example/software/controller/Controller.java
+++ src/main/java/com/example/software/controller/Controller.java
@@ -38,7 +38,6 @@ public class Controller {
             return new ResponseEntity<>("删除任务时出错: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     @PostMapping("/updateTask")
     public ResponseEntity<String> updateTask(@RequestBody Task editedTask) {
         try {
